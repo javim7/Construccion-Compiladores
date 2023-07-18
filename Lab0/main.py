2f0bf7d9f1fca8112f8b409db24058b9778c06f8
Javier Mombiela <61723252+javim7@users.noreply.github.com>
2023-07-18 14:10:06 -0600

    gramatica YAPL completa

---
+++ Lab0/main.py
@@ -0,0 +1,33 @@
+from antlr4 import *
+from YAPLLexer import YAPLLexer
+from YAPLParser import YAPLParser
+
+class YAPLListener(ParseTreeListener):
+    def enterEveryRule(self, ctx):
+        print("Enter", type(ctx).__name__)
+
+    def exitEveryRule(self, ctx):
+        print("Exit", type(ctx).__name__)
+
+def main():
+    # Cargar el archivo de YAPL
+    input_file = "Lab0/ejemplo2.yapl"  # Reemplaza con tu propio archivo de YAPL
+    input_stream = FileStream(input_file)
+
+    # Crear el lexer
+    lexer = YAPLLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+
+    # Crear el parser
+    parser = YAPLParser(token_stream)
+
+    # Obtener el árbol de análisis sintáctico
+    tree = parser.program()
+
+    # Agregar un listener personalizado para mostrar los nodos del árbol de análisis sintáctico
+    listener = YAPLListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+
+if __name__ == '__main__':
+    main()
