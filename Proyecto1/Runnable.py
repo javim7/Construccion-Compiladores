fc57dbf23848b7196a6229ef07fa27446728739e
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2023-08-27 18:54:32 -0600

    quitando scrollbar sincronizado

--- Proyecto1/Runnable.py
+++ Proyecto1/Runnable.py
@@ -22,22 +22,18 @@ def redraw():
 
 root = tk.Tk()
 root.title("IDE")
-root.geometry("800x800")
+root.geometry("800x600")
 
-# Scrollbar
-scrollbar = Scrollbar(root)
-scrollbar.pack(side="right", fill="y")
+# Hacer la ventana no redimensionable
+root.resizable(False, False)
 
 # Canvas para números de línea
 line_numbers_canvas = tk.Canvas(root, width=30)
 line_numbers_canvas.pack(side="left", fill="y", padx=(5, 0))
 
 # Área de código con espacio entre líneas
-code_area = Text(root, wrap="none", undo=True, yscrollcommand=scrollbar.set, spacing1=5)
+code_area = Text(root, wrap="none", undo=True, spacing1=5)
 code_area.pack(pady=20, padx=20, expand=True, fill="both")
-
-# Conectar scrollbar
-# scrollbar.config(command=code_area.yview)
 
 # Botón de compilar
 compile_button = tk.Button(root, text="Compilar", command=compile_code)
@@ -46,5 +42,4 @@ compile_button.pack(pady=20)
 # Llamada inicial para mostrar números de línea
 redraw()
 
-root.resizable(False, False)
 root.mainloop()
